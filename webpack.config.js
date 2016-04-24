4e3731a466d392e0a1ce966926f1e60ee8f634a6
rjmacarthy <richardmacarthy@hotmail.com>
2016-04-24 22:13:51 +0100

    testing

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+// webpack.config.dev.js
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        './public/js/main'
+    ],
+    output: {
+        path: path.join(__dirname, 'public/dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js']
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ],
+    stats: {
+        colors: true
+    },
+    module: {
+        loaders: [{
+            test: /\.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015']
+            }
+        }]
+    }
+};
